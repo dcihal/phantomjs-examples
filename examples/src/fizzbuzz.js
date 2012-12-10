35fd7d31b6516133a265e203967eb82e347a65a0
Noppanit Charassinvichai <noppanit.c@gmail.com>
2012-12-10 23:07:48 +0100

    Added shortened version of fizzbuzz

--- examples/src/fizzbuzz.js
+++ examples/src/fizzbuzz.js
@@ -5,16 +5,27 @@ var FizzBuzz = function() {
 FizzBuzz.prototype = {
 
 	getResult : function(number) {
-		if(number % 3 == 0 && number % 5 == 0 && number != 0)
-			return "fizzbuzz";
+		var i = number,
+			f = 'fizz',
+			b = 'buzz',
+			out = '';
 
-		if(number % 3 == 0 && number != 0)
-			return "fizz";
+		if(number === 0)
+			return '0';
 
-		if(number % 5 == 0 && number != 0)
-			return 'buzz';
+		out = !(i % 3) ?  !(i % 5)? f+b : f : !(i % 5)? b : i;
+		return out.toString();
 
-		return number.toString();
+		// if(number % 3 == 0 && number % 5 == 0 && number != 0)
+		// 	return "fizzbuzz";
+
+		// if(number % 3 == 0 && number != 0)
+		// 	return "fizz";
+
+		// if(number % 5 == 0 && number != 0)
+		// 	return 'buzz';
+
+		// return number.toString();
 	}
 
 }
